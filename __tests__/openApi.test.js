6749509269384225a0b8db50b9cb514b5981474e
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-07-13 09:51:01 +0300

    jest-openapi

--- __tests__/openApi.test.js
+++ __tests__/openApi.test.js
@@ -1,17 +1,49 @@
-import axios from 'axios'
 import jestOpenAPI from 'jest-openapi';
+import axios from 'axios';
 
-jestOpenAPI(`${__dirname}/api-description.yml`);
 
-describe('GET /nodes', () => {
-  it('should satisfy OpenAPI spec', async () => {
+describe('/nodes', () => {
+  jestOpenAPI(`${__dirname}/../src/api-description.yml`);
+  let newNode = {};
 
-    // Get an HTTP response from your server (e.g. using axios)
-    const res = await axios.get('http://localhost:5000/nodes');
+  it('GET', async () => {
+    const res = await axios.get('http://localhost:5000/nodes?parentId=1');
+    expect(res.status).toEqual(200);
+  });
+
+  it('POST', async () => {
+    const res = await axios.post('http://localhost:5000/nodes', {
+      ip: '1.1.1.1',
+      port: 234,
+      name: '12212',
+      parentId: null,
+    });
+    newNode = res.data;
 
     expect(res.status).toEqual(200);
+    expect(res.data).toSatisfySchemaInApiSpec('Node');
+  });
 
-    // Assert that the HTTP response satisfies the OpenAPI spec
-    expect(res).toSatisfyApiSpec();
+  it('POST 400', async () => {
+    try {
+      await axios.post('http://localhost:5000/nodes', {
+        ip: '1.1.1.1',
+        port: 234,
+        name: '12212',
+        parentId: null,
+      });
+    } catch (e) {
+      expect(e.response.status).toEqual(400);
+      expect(e.response.data).toSatisfySchemaInApiSpec('Error');
+    }
+  });
+
+  it('DELETE', async () => {
+    const res = await axios.delete('http://localhost:5000/nodes', {
+      data: {
+        id: newNode.id,
+      },
+    });
+    expect(res.status).toEqual(200);
   });
 });
